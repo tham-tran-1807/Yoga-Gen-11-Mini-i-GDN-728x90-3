03281def84c7ef8d4331d184f66659b34c8c06b5
tham-tran-1807 <wildwolfnet247@gmail.com>
2026-07-28 11:43:53 +0700

    Add files via upload

---
+++ Yoga-Gen-11-Mini-i-GDN-728x90.js
@@ -0,0 +1,487 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [
+		{name:"Yoga_Gen_11_Mini_i_GDN_728x90_atlas_1", frames: [[730,0,50,50],[0,0,728,90],[0,644,503,90],[0,368,594,90],[0,552,505,90],[0,276,635,90],[0,736,501,90],[0,460,585,90],[0,828,357,90],[0,92,728,90],[0,184,709,90]]}
+];
+
+
+(lib.AnMovieClip = function(){
+	this.actionFrames = [];
+	this.ignorePause = false;
+	this.gotoAndPlay = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndPlay.call(this,positionOrLabel);
+	}
+	this.play = function(){
+		cjs.MovieClip.prototype.play.call(this);
+	}
+	this.gotoAndStop = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndStop.call(this,positionOrLabel);
+	}
+	this.stop = function(){
+		cjs.MovieClip.prototype.stop.call(this);
+	}
+}).prototype = p = new cjs.MovieClip();
+// symbols:
+
+
+
+(lib.arrows_w = function() {
+	this.initialize(ss["Yoga_Gen_11_Mini_i_GDN_728x90_atlas_1"]);
+	this.gotoAndStop(0);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.BG = function() {
+	this.initialize(ss["Yoga_Gen_11_Mini_i_GDN_728x90_atlas_1"]);
+	this.gotoAndStop(1);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.F1 = function() {
+	this.initialize(ss["Yoga_Gen_11_Mini_i_GDN_728x90_atlas_1"]);
+	this.gotoAndStop(2);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.F2 = function() {
+	this.initialize(ss["Yoga_Gen_11_Mini_i_GDN_728x90_atlas_1"]);
+	this.gotoAndStop(3);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.F3 = function() {
+	this.initialize(ss["Yoga_Gen_11_Mini_i_GDN_728x90_atlas_1"]);
+	this.gotoAndStop(4);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.F4 = function() {
+	this.initialize(ss["Yoga_Gen_11_Mini_i_GDN_728x90_atlas_1"]);
+	this.gotoAndStop(5);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.F5 = function() {
+	this.initialize(ss["Yoga_Gen_11_Mini_i_GDN_728x90_atlas_1"]);
+	this.gotoAndStop(6);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.F6 = function() {
+	this.initialize(ss["Yoga_Gen_11_Mini_i_GDN_728x90_atlas_1"]);
+	this.gotoAndStop(7);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.F7 = function() {
+	this.initialize(ss["Yoga_Gen_11_Mini_i_GDN_728x90_atlas_1"]);
+	this.gotoAndStop(8);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.F8111 = function() {
+	this.initialize(ss["Yoga_Gen_11_Mini_i_GDN_728x90_atlas_1"]);
+	this.gotoAndStop(9);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Logo = function() {
+	this.initialize(ss["Yoga_Gen_11_Mini_i_GDN_728x90_atlas_1"]);
+	this.gotoAndStop(10);
+}).prototype = p = new cjs.Sprite();
+// helper functions:
+
+function mc_symbol_clone() {
+	var clone = this._cloneProps(new this.constructor(this.mode, this.startPosition, this.loop, this.reversed));
+	clone.gotoAndStop(this.currentFrame);
+	clone.paused = this.paused;
+	clone.framerate = this.framerate;
+	return clone;
+}
+
+function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
+	var prototype = cjs.extend(symbol, cjs.MovieClip);
+	prototype.clone = mc_symbol_clone;
+	prototype.nominalBounds = nominalBounds;
+	prototype.frameBounds = frameBounds;
+	return prototype;
+	}
+
+
+(lib.nutreplay = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#0066CC").s().p("AhhBiIAAjDIDDAAIAADDg");
+	this.shape.setTransform(9.8,9.8);
+	this.shape._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(3).to({_off:false},0).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,19.6,19.6);
+
+
+(lib.muitenxoay = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_1
+	this.instance = new lib.arrows_w();
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = getMCSymbolPrototype(lib.muitenxoay, new cjs.Rectangle(0,0,50,50), null);
+
+
+(lib.Chamuitenxoay = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// hinh_mui_ten
+	this.instance = new lib.muitenxoay();
+	this.instance.setTransform(14.55,10.25,0.31,0.31,0,0,0,25,25);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).to({rotation:-360},12).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(3.9,-0.4,21.3,21.299999999999997);
+
+
+// stage content:
+(lib.YogaGen11MiniiGDN728x90 = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = false; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	this.actionFrames = [373];
+	// timeline functions:
+	this.frame_373 = function() {
+		this.mc_mui_ten_xoay.stop();
+		
+		
+		
+		/* Mouse Over Event
+		Mousing over the symbol instance executes a function in which you can add your own custom code.
+		
+		Instructions:
+		1. Add your custom code on a new line after the line that says "// Start your custom code" below.
+		The code will execute when the symbol instance is moused over.
+		frequency is the number of the times event should be triggered.
+		*/
+		var frequency = 5;
+		stage.enableMouseOver(frequency);
+		this.btn_replay.addEventListener("mouseover", fl_MouseOverHandler.bind(this));
+		
+		function fl_MouseOverHandler()
+		{
+			// Start your custom code
+			// This example code displays the words "Moused over" in the Output panel.
+			this.mc_mui_ten_xoay.play();
+			// End your custom code
+		}
+		
+		
+		
+		/* Mouse Out Event
+		Mousing out of the symbol instance executes a function in which you can add your own custom code.
+		
+		Instructions:
+		1. Add your custom code on a new line after the line that says "// Start your custom code" below.
+		The code will execute when the symbol instance is moused out of.
+		*/
+		var frequency = 5;
+		stage.enableMouseOver(frequency);
+		this.btn_replay.addEventListener("mouseout", fl_MouseOutHandler_4.bind(this));
+		
+		function fl_MouseOutHandler_4()
+		{
+			// Start your custom code
+			// This example code displays the words "Moused out" in the Output panel.
+			this.mc_mui_ten_xoay.gotoAndStop(0);
+			// End your custom code
+		}
+		
+		/* Mouse Click Event
+		Clicking on the specified symbol instance executes a function in which you can add your own custom code.
+		
+		Instructions:
+		1. Add your custom code on a new line after the line that says "// Start your custom code" below.
+		The code will execute when the symbol instance is clicked.
+		*/
+		
+		this.btn_replay.addEventListener("click", fl_MouseClickHandler.bind(this));
+		
+		function fl_MouseClickHandler()
+		{
+			globalThis.notRedirect=true;
+			// Start your custom code
+			// This example code displays the words "Mouse clicked" in the Output panel.
+			this.gotoAndPlay(0);
+			// End your custom code
+		}
+	}
+
+	// actions tween:
+	this.timeline.addTween(cjs.Tween.get(this).wait(373).call(this.frame_373).wait(17));
+
+	// nut_ao_replay
+	this.btn_replay = new lib.nutreplay();
+	this.btn_replay.name = "btn_replay";
+	this.btn_replay.setTransform(13.8,9.8,1,1,0,0,0,9.8,9.8);
+	this.btn_replay._off = true;
+	new cjs.ButtonHelper(this.btn_replay, 0, 1, 2, false, new lib.nutreplay(), 3);
+
+	this.timeline.addTween(cjs.Tween.get(this.btn_replay).wait(372).to({_off:false},0).wait(18));
+
+	// hinh_mui_ten
+	this.mc_mui_ten_xoay = new lib.Chamuitenxoay();
+	this.mc_mui_ten_xoay.name = "mc_mui_ten_xoay";
+	this.mc_mui_ten_xoay.setTransform(13.8,-7.8,1,1,0,0,0,14.6,10.2);
+	this.mc_mui_ten_xoay._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.mc_mui_ten_xoay).wait(360).to({_off:false},0).to({y:10.45},12).wait(18));
+
+	// Logo
+	this.instance = new lib.Logo();
+	this.instance.setTransform(19,0);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(390));
+
+	// F8
+	this.instance_1 = new lib.F8111();
+	this.instance_1._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_1).wait(350).to({_off:false},0).wait(40));
+
+	// F7
+	this.instance_2 = new lib.F7();
+	this.instance_2.setTransform(277,0);
+	this.instance_2._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_2).wait(300).to({_off:false},0).to({_off:true},50).wait(40));
+
+	// F6
+	this.instance_3 = new lib.F6();
+	this.instance_3._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_3).wait(249).to({_off:false},0).to({_off:true},51).wait(90));
+
+	// F5
+	this.instance_4 = new lib.F5();
+	this.instance_4.setTransform(137,0);
+	this.instance_4._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_4).wait(199).to({_off:false},0).to({_off:true},50).wait(141));
+
+	// F4
+	this.instance_5 = new lib.F4();
+	this.instance_5._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_5).wait(149).to({_off:false},0).to({_off:true},50).wait(191));
+
+	// F3
+	this.instance_6 = new lib.F3();
+	this.instance_6.setTransform(122,0);
+	this.instance_6._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_6).wait(100).to({_off:false},0).to({_off:true},49).wait(241));
+
+	// F2
+	this.instance_7 = new lib.F2();
+	this.instance_7._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_7).wait(50).to({_off:false},0).to({_off:true},50).wait(290));
+
+	// F1
+	this.instance_8 = new lib.F1();
+	this.instance_8.setTransform(128,0);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_8).to({_off:true},50).wait(340));
+
+	// BG
+	this.instance_9 = new lib.BG();
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_9).wait(390));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new lib.AnMovieClip();
+p.nominalBounds = new cjs.Rectangle(364,29.5,364,60.5);
+// library properties:
+lib.properties = {
+	id: '77BE86E86AD5CD4EA4DA40BFF88C7F02',
+	width: 728,
+	height: 90,
+	fps: 24,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [
+		{src:"images/Yoga_Gen_11_Mini_i_GDN_728x90_atlas_1.png", id:"Yoga_Gen_11_Mini_i_GDN_728x90_atlas_1"}
+	],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['77BE86E86AD5CD4EA4DA40BFF88C7F02'] = {
+	getStage: function() { return exportRoot.stage; },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+an.makeResponsive = function(isResp, respDim, isScale, scaleType, domContainers) {		
+	var lastW, lastH, lastS=1;		
+	window.addEventListener('resize', resizeCanvas);		
+	resizeCanvas();		
+	function resizeCanvas() {			
+		var w = lib.properties.width, h = lib.properties.height;			
+		var iw = window.innerWidth, ih=window.innerHeight;			
+		var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;			
+		if(isResp) {                
+			if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {                    
+				sRatio = lastS;                
+			}				
+			else if(!isScale) {					
+				if(iw<w || ih<h)						
+					sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==1) {					
+				sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==2) {					
+				sRatio = Math.max(xRatio, yRatio);				
+			}			
+		}
+		domContainers[0].width = w * pRatio * sRatio;			
+		domContainers[0].height = h * pRatio * sRatio;
+		domContainers.forEach(function(container) {				
+			container.style.width = w * sRatio + 'px';				
+			container.style.height = h * sRatio + 'px';			
+		});
+		stage.scaleX = pRatio*sRatio;			
+		stage.scaleY = pRatio*sRatio;
+		lastW = iw; lastH = ih; lastS = sRatio;            
+		stage.tickOnUpdate = false;            
+		stage.update();            
+		stage.tickOnUpdate = true;		
+	}
+}
+an.handleSoundStreamOnTick = function(event) {
+	if(!event.paused){
+		var stageChild = stage.getChildAt(0);
+		if(!stageChild.paused || stageChild.ignorePause){
+			stageChild.syncStreamSounds();
+		}
+	}
+}
+an.handleFilterCache = function(event) {
+	if(!event.paused){
+		var target = event.target;
+		if(target){
+			if(target.filterCacheList){
+				for(var index = 0; index < target.filterCacheList.length ; index++){
+					var cacheInst = target.filterCacheList[index];
+					if((cacheInst.startFrame <= target.currentFrame) && (target.currentFrame <= cacheInst.endFrame)){
+						cacheInst.instance.cache(cacheInst.x, cacheInst.y, cacheInst.w, cacheInst.h);
+					}
+				}
+			}
+		}
+	}
+}
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
